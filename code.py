df24d1d7ed9d96ee8132910c355346f1bb8a0757
Andrea Gonzato <gonzatoandrea98@gmail.com>
2021-04-24 12:00:39 +0200

    update code

--- code.py
+++ code.py
@@ -1,48 +1,62 @@
-def standard():
-    A = [2, -7, 8, 3, -5, -5, 9, 1, 12, 4]
+def exercice2b(A):
     B = [0 for i in range(0, len(A))]
     for i in range(0, len(A)):
-        cout = 0
+        count = 0
         for j in range(i+1, len(A)):
             if A[j] < A[i]:
-                cout += 1
-        B[i] = cout
-    print(A)
-    print(B)
-
-def zero_version():
-    A = [0, -7, 0, 1, 0, 0, 0, -3, 8, 0]
-    B = [-1 for i in range(0, len(A))]
-    index_non_zero = []
+                count += 1
+        B[i] = count
+    return B
 
 
+def exercice2c(A):
+    B = [0 for i in range(0, len(A))]
+    non_zero_indexes = []
     negative_left_values = 0
+
     # count negative_left_values
     for i in range(0, len(A)):
         if A[i] < 0:
             negative_left_values += 1
 
-    # manage all the case where A[i] = 0
+    # manage all the cases where A[i] = 0
     for i in range(0, len(A)):
         if A[i] == 0:
             B[i] = negative_left_values
         if A[i] < 0:
             negative_left_values -= 1
         if A[i] != 0:
-            index_non_zero.append(i)
+            non_zero_indexes.append(i)
 
-    # loop for a constant time
-    for i in range(0, len(index_non_zero)):
-        cout = 0
-        for j in range(index_non_zero[i] + 1, len(A)):
-            if A[j] < A[index_non_zero[i]]:
-                cout += 1
-        B[index_non_zero[i]] = cout
+    # manage the cases where A[i] != 0
+    for i in range(0, len(non_zero_indexes)):
+        count = 0
+        for j in range(non_zero_indexes[i] + 1, len(A)):
+            if A[j] < A[non_zero_indexes[i]]:
+                count += 1
+        B[non_zero_indexes[i]] = count
 
-    print(A)
-    print(B)
+    return B
 
 if __name__ == '__main__':
-    zero_version()
+    A = [2, -7, 8, 3, -5, -5, 9, 1, 12, 4]
+    B = exercice2b(A)
+    print("B =", B)
 
 
+    print("----------------------")
+
+
+    A_zeros = [0, -7, 0, 1, 0, 0, 0, -3, 8, 0]
+    B_zeros = exercice2c(A_zeros)
+    print("B_zeros = ", B_zeros)
+
+
+
+
+'''
+B = [4, 0, 5, 3, 0, 0, 2, 0, 1, 0]
+----------------------
+B_zeros =  [2, 0, 1, 5, 1, 1, 1, 0, 1, 0]
+
+'''
